201794e0e154b941033ad65f5c345ece2618055a
RishabhSE <rishabhsengar12345@gmail.com>
2018-08-25 12:42:43 +0530

    Templates

--- Shrinkage Methods.R
+++ Shrinkage Methods.R
@@ -26,15 +26,63 @@ x = model.matrix( Salary~. ,Hitters)[ ,-1 ]
 y = Hitters$Salary
 
 # *** RIDGE REGRESSION ***
+# Obective = RSS + lambda*(sum of sq. of mag. of Coeff.)
 
-grid = 10^seq( from = 10,to = -2, length = 100)
-ridge.mod = glmnet( x, y, alpha = 0, lambda = grid )
+grid = 10^seq( from = 10,to = -2, length = 100)# Lamda values in range(particular)
+ridge.mod = glmnet( x, y, alpha = 0, lambda = grid )# alpha---> which model ?
 # glmne() : It Standardize the variables on same scale( By default )
 
 # dim( coef( ridge.mod))
-ridge.mod$lambda[ 50 ]; coef( ridge.mod)[ ,50 ]
-# Coeff. Estimates to be much smaller( l2 norms )
+ridge.mod$lambda[50]
+# We excepts Coeff. Estimates to be much smaller( L2 norms )
 # when : large value of lambda is used compared to small(lambda) value
+
+coef( ridge.mod)[,50]
+# Coeff. when lambda resulted in previous step
+
+#Esimation test error of R.R  *********************
+# Splitting
+set.seed(1)
+train=sample( 1:nrow(x) ,size = nrow(x)/2)
+test =( -train )
+y.test = y[test]
+
+# Fitting R.R on training set
+rigid.mod = glmnet( x[train] ,y[train] ,alpha = 0, lambda = grid,
+                    thresh = 1e-12)
+# Prediction using lambda = 4
+rigid.pred = predict( rigid.mod ,s=4, newx = x[test ,])
+
+# Calculation of test MSE
+mean(( rigid.pred - y.test)^2)
+
+# As we know that : *******************************
+# at lambda = 0 : model acts as least sq. model
+# at lambda = 1e10 : model acts as NULL model containing only intercept
+
+# M-1
+# test MSE with just an intercept
+mean((mean( y[train]) -y.test)^2)
+# For Large value
+rigid.pred = predict( rigid.mod ,s= 1e10, newx = x[test ,])
+mean(( rigid.pred - y.test)^2)
+# same MSE as O/P
+
+# M-2
+# test MSE with least sq. model
+lm( y~x , subset = train)
+predict( rigid.mod, s=0 ,exact=T, type ="coefficients")[1:20, ]
+# For small( ZERO ) value
+rigid.pred = predict( rigid.mod ,s= 0, newx = x[test ,])
+mean(( rigid.pred - y.test)^2)
+# same MSE as O/P
+
+# 
+
+
+
+
+
 
 
 
