210c4247ec91ae7d745ede09f5963aea47f8a3ed
RishabhSE <rishabhsengar12345@gmail.com>
2018-09-10 16:30:18 +0530

    added nothing

--- MOVIES_DATA.R
+++ MOVIES_DATA.R
@@ -5,18 +5,18 @@ library("doParallel")
 
 # Create your computing cluster
 # "detectCores" fn. will return the number of cores in your computer
-cl <- makeCluster(detectCores() - 1)
-registerDoParallel(cl, cores = detectCores() - 1)
+cl <- makeCluster(detectCores() - 2)
+registerDoParallel(cl, cores = detectCores() - 2)
 
-data = foreach(i = 2:131262, .packages = 
+data = foreach(i = 1:100, .packages = 
                  c("ncdf4","chron","stats"),
                .combine = rbind) %dopar% {
                  try({
-                   df_rating_exp = filter(df_rating, movieid == i)
-                   rating_avg = sum(df_rating_exp$rating)/nrow(df_rating_exp)
-                   movieid = i
-                   df_rating_merge = data.frame(movieid , rating_avg)
-                   df_new = rbind(df_new ,df_rating_merge)
+                   data_exp = filter(data_set, anime_id == i )
+                   rating_median = median(data_exp$rating)
+                   anime_id = i
+                   df_new_n = data.frame(anime_id , rating_median)
+                   df_new = rbind(df_new ,df_new_n)
                  })
                }
 
@@ -25,22 +25,42 @@ stopCluster(cl)
 # *********************************************************
 library(dplyr)
 
-data_set = data.frame(rating$movieId,rating$rating)
+data_set = data.frame(rating)
+data_set = arrange(data_set,data_set$user_id)
 
-df_rating = arrange(data_set,data_set$rating.movieId)
-colnames(df_rating) = c("movieid" , "rating")
+# Counting user not given rating 
+sum(data_set[,3] == -1 )
 
-df_rating_exp = filter(df_rating, movieid == 1)
-rating_avg = sum(df_rating_exp$rating)/nrow(df_rating_exp)
-movieid = 1
-df_new = data.frame(movieid , rating_avg)
+# Removing them
+data_set = filter(data_set, rating != -1)
 
-for (i in 2:500) 
+data_exp = filter(data_set, anime_id ==1 )
+rating_median = median(data_exp$rating)
+anime_id = 1
+df_new = data.frame(anime_id , rating_median)
+
+for (i in 1:34475) 
 {
-  df_rating_exp = filter(df_rating, movieid == i)
-  rating_avg = sum(df_rating_exp$rating)/nrow(df_rating_exp)
-  movieid = i
-  df_rating_merge = data.frame(movieid , rating_avg)
-  df_new = rbind(df_new ,df_rating_merge)
+  data_exp = filter(data_set, anime_id == i )
+  rating_median = median(data_exp$rating)
+  anime_id = i
+  df_new_n = data.frame(anime_id , rating_median)
+  df_new = rbind(df_new ,df_new_n)
 }
+
+# sum( is.na( df_new$rating_median )) [Count no. of missing values]
+
+for (i in 2:100) 
+{
+  if(is.na( is.na(df_new[4,2]) ))
+  {
+    data_exp = filter(data_set, anime_id == 4 )
+    rating_median = mean(data_exp$rating)
+    anime_id = 4
+    df_new_n = data.frame(anime_id , rating_median)
+    df_new = rbind(df_new ,df_new_n)
+  }
+}
+
+# *********************************************************
 
